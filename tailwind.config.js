08ce4158aa5593b457d7e758be226fa7f641b117
Facely1er <facelykande@gmail.com>
2025-06-23 23:34:16 -0400

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ export default {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#FF6B00", // Updated to use #FF6B00 as primary
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
