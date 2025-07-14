ce67d91b42c699aec17632b89c89189017992474
Facely1er <facelykande@gmail.com>
2025-07-14 12:04:55 -0400

    Fix visualizer plugin build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({
-      open: true, // Automatically open the bundle report after build
-      filename: 'dist/stats.html',
-      gzipSize: true,
-      brotliSize: true,
-    }),
   ],
   resolve: {
     alias: {
